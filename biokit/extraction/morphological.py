b42f1335ebe7d2aa6609fc2ab62fef1166811176
Weslley <weslleylc@gmail.com>
2023-02-15 14:03:07 -0300

    add morphological features boilerplate

---
+++ biokit/extraction/morphological.py
@@ -0,0 +1,11 @@
+
+
+def get_morphological_waves(ecg_signal, sampling_rate, **kwargs):
+    """return the list of nn intervals.
+    Args:
+        ecg_signal: Cleaned ecg signal.
+        sampling_rate: The sampling rate of the signal.
+    Returns:
+        The dictionary with all morphological waves
+    """
+    pass
